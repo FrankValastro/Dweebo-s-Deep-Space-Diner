c390624f52b2273cb6d7046b38628c326ecaf4a2
Mathew Emslie <mathewemslie@gmail.com>
2026-06-30 11:03:06 +1000

    Fixed GitIgnore

---
+++ Dweebo's-Deep-Space-Diner/Assets/Scripts/Button Test.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ButtonTest : MonoBehaviour
+{
+public Button TestButton;
+public Canvas canvas;
+public Image Image;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void OnButtonPressed()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
